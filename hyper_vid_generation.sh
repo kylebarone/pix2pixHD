2e4f1e6b939386d594bc6713e60771ea7cbbea04
Kyle Barone <45906831+kylebarone@users.noreply.github.com>
2021-04-26 13:24:14 -0400

    Fixed error

--- hyper_vid_generation.sh
+++ hyper_vid_generation.sh
@@ -12,7 +12,7 @@ echo "-----------------Generating EPOCH 40-----------------"
 python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 10 --how_many $1 --dont_crop_720
 
 echo "-----------------Generating EPOCH 80-----------------"
-python3 generate_video.py --name v --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 
+python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 
 
 echo "-----------------Generating EPOCH 80-----------------"
-python3 generate_video.py --name v --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 --start_from ~/test_rainbow.jpg
+python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 --start_from ~/test_rainbow.jpg
