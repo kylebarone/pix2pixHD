d80fc58359975131672493b208d98c1fe95cf1d3
kbarone6 <kbarone@gatech.edu>
2021-04-30 20:40:51 -0400

    modified script

--- hyper_vid_generation.sh
+++ hyper_vid_generation.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-echo "Starting the script file that runs and creates $1 second videos"
 
-echo "-----------------Generating EPOCH 10-----------------"
-python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 1 --how_many $1 --dont_crop_720 
+echo "-----------------Generating Zoom 1,-----------------"
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 1 --extra_output_dir zoom_expierement
 
-echo "-----------------Generating EPOCH 20-----------------"
-python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 5 --how_many $1 --dont_crop_720 
+echo "-----------------Generating Zoom 1-----------------"
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 2 --extra_output_dir zoom_expierement
 
-echo "-----------------Generating EPOCH 40-----------------"
-python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 10 --how_many $1 --dont_crop_720
-
-echo "-----------------Generating EPOCH 80-----------------"
-python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 
-
-echo "-----------------Generating EPOCH 80-----------------"
-python3 generate_video.py --name hyper_base --dataroot ./datasets/hyper_dataset/ --fps 24 --ngf 32 --which_epoch 20 --how_many $1 --dont_crop_720 --start_from ~/test_rainbow.jpg
+echo "-----------------Generating Zoom 1,-----------------"
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 1 --extra_output_dir zoom_expierement --start_from ~/test_rainbow.jpg
