170a5ce4c72e99719e377f7cffcaa64d0b0768fb
kbarone6 <kbarone@gatech.edu>
2021-05-01 13:15:53 -0400

    new script

--- hyper_vid_generation.sh
+++ hyper_vid_generation.sh
@@ -2,10 +2,10 @@
 
 
 echo "-----------------Generating Zoom 1,-----------------"
-python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 1 --extra_output_dir zoom_expierement
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 3 --extra_output_dir zoom_expierement
 
 echo "-----------------Generating Zoom 1-----------------"
-python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 2 --extra_output_dir zoom_expierement
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 3 --extra_output_dir zoom_expierement --heat_seeking_lvl  2
 
 echo "-----------------Generating Zoom 1,-----------------"
-python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 1 --extra_output_dir zoom_expierement --start_from ~/test_rainbow.jpg
+python3 generate_video.py --name light --dataroot ./datasets/light_dataset/ --fps 24 --ngf 32 --which_epoch 176 --how_many 600 --dont_crop_720 --zoom_lvl 3 --extra_output_dir zoom_expierement --start_from ~/test_rainbow.jpg
